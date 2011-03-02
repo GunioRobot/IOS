c3e56fe86fb1a9e7a43c9742a822ca78d2bbc80d
Michael McGlynn <michael.mcglynn@tequila.com.au>
2011-03-02 17:59:53 +1100

    Assignment 5: Project set up and examples added
    Signed-off-by: Michael McGlynn <michael.mcglynn@tequila.com.au>

---
+++ Week 05/Places/Classes/PlacesAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PlacesAppDelegate.h
+//  Places
+//
+//  Created by Michael McGlynn on 2/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
